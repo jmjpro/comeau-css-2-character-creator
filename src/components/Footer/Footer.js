aff09f0793ef73c2d56b8b927f4392591e130451
Joshua Jacobson <joshuaj@ziprecruiter.com>
2024-03-19 14:29:38 +0200

    feat: character creation workshop

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import styles from './Footer.module.css';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      All character assets taken from{' '}
+      All character assets taken from' '
       <a
         href="https://www.openpeeps.com/"
         target="_blank"
